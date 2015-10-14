6473648e9ed4ba48fa592926ecaa021f4d89eb50
eziante <kalleladefogedpedersen@gmail.com>
2015-10-14 11:55:38 +0200

    Fixed a problem with included cs file

---
+++ src/GLF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericLoginFramework
+{
+    class GLF
+    {
+        public GLF()
+        {
+        }
+
+        public void EnableOauthProvider()
+        {
+
+        }
+
+        public void EnableOpenIDProvider()
+        {
+
+        }
+    }
+}
