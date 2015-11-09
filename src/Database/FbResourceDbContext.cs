f53030bc26dcbe97c03860e92177f409d331528e
iyiyy <189584@via.dk>
2015-11-09 20:27:33 +0100

    added FbResourceDbContext class added UserDbContext class added User class

---
+++ src/Database/FbResourceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace GenericLoginFramework.Database
+{
+    class FbResourceDbContext : DbContext
+    {
+        public FbResourceDbContext() : base()
+        {
+            Database.Initialize(true);
+        }
+
+        public FbResourceDbContext(string name, bool connStringName)
+            : base(connStringName ? String.Format("name={0}", name) : name)
+        {
+            Database.Initialize(true);
+        }
+
+        public DbSet<FacebookResource> FacebookResources { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types().Configure(entity => entity.ToTable("glf." + entity.ClrType.Name);
+        }
+    }
+}
