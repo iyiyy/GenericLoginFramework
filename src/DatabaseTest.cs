82ba6ebe2a7e4b058f95a08f7ff50c70503bbe41
iyiyy <189584@via.dk>
2015-11-23 09:54:35 +0100

    Solved key issues on database creation

---
+++ src/DatabaseTest.cs
@@ -0,0 +1,28 @@
+﻿using GenericLoginFramework.Database;
+
+namespace GenericLoginFramework
+{
+    class DatabaseTest
+    {
+        static void Main(string[] args)
+        {
+            testGenericLoginFrameworkDbContext();
+
+            System.Console.WriteLine("testing done, press any key to exit...");
+            System.Console.ReadKey();
+        }
+
+        private static void testGenericLoginFrameworkDbContext()
+        {
+            System.Console.WriteLine("Testing database creation");
+
+            using (GLFDbContext db = new GLFDbContext("shit", false))
+            {
+                //db.Users.Add(new User { ID = "glfUser" });
+                //db.FacebookResources.Add(new OAuth.Resources.FacebookResource { ID = "GlfFbUser", UserID = "glfUser" });
+                //db.GoogleResources.Add(new OpenID.Resources.GoogleResource { ID = "GlfGUser", UserID = "glfUser" });
+                //db.SaveChanges();
+            }
+        }
+    }
+}
--- src/OpenID/Resources/GoogleResource.cs
+++ src/OpenID/Resources/GoogleResource.cs
@@ -1,10 +1,14 @@
-﻿namespace GenericLoginFramework.OpenID.Resources
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GenericLoginFramework.OpenID.Resources
 {
     public class GoogleResource : OpenIDResource
     {
         public override string ID { get; set; }
 
-        public string UserID { get; set; }
+        //public string UserID { get; set; }
+        [Key, ForeignKey("User")]
         public virtual User User { get; set; }
     }
 }
