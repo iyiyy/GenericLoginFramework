d02cba53202f2d7dd0bd05f7dfafb1a02b91c015
Kenni Rau <kennirau@gmail.com>
2015-12-04 14:27:52 +0100

    Added WPF Buttons

---
+++ src/GLFToolboxWPF/Generic.xaml.cs
@@ -0,0 +1,27 @@
+﻿using GenericLoginFramework;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GLFToolboxWPF
+{
+    /// <summary>
+    /// Interaction logic for Generic.xaml.
+    /// </summary>
+    [ProvideToolboxControl("GLFToolboxWPF.Generic", true)]
+    public partial class Generic : UserControl
+    {
+        public User User { private set; get; }
+
+        public Generic()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //User = GLF.LoginWithGeneric(usernameBox.Text, passwordBox.Password);
+        }
+    }
+}
+}
