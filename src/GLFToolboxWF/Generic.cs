a5aef0f888cad7cdd40ba140885c1e6c030c26df
Kenni Rau <kennirau@gmail.com>
2015-12-08 15:26:55 +0100

    Los Buttons :P

---
+++ src/GLFToolboxWF/Generic.cs
@@ -0,0 +1,23 @@
+﻿using GenericLoginFramework;
+using System.Globalization;
+using System;
+using System.Windows.Forms;
+
+namespace GLFToolboxWF
+{
+    [ProvideToolboxControl("GLFToolboxWF.Generic", false)]
+    public partial class Generic : UserControl
+    {
+        public User User { private set; get; }
+
+        public Generic()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            //User = GLF.LoginWithGeneric(UsernameTxtBx.Text, PasswordTxtBx.Text);
+        }
+    }
+}
